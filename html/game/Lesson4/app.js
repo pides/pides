697ed843cf11cdbc46eacff360e405d7c540926a
pides <pides_h@126.com>
2015-09-22 22:35:41 +0800

    'btsize'

--- html/game/Lesson4/app.js
+++ html/game/Lesson4/app.js
@@ -8,6 +8,7 @@
       guide = 2,
       guideHeight = 80,
       menuBtnOffset = 6.5;
+
   function stage(t) {
     this.t = t;
     this.init();
@@ -15,37 +16,50 @@
   }
 
   stage.prototype = {
+    _maxStageWidth: 480,
+    multiple: 1,
     init: function () {
       this.stageWidth = parseFloat(this.t.width);
       this.stageHeight = parseFloat(this.t.height);
+      this.reSetSize();
       this.Stage = new createjs.Stage(this.t);
       this._bindEvent();
-      this.reSetSize();
     },
     reSetSize: function () {
       var client = this._getClient();
-      if(this.stageHeight>client.height){
+      if (this.stageHeight > client.height) {
         this.stageHeight = this.t.height = client.height;
-        alert(client.width);
         this.t.parentNode.style.height = client.height + 'px';
       }
-    },
+      if (client.width < this._maxStageWidth) {
+        this.multiple = client.width / this._maxStageWidth;
+        this.stageWidth = this.t.width = this._maxStageWidth * this.multiple;
+        this.t.parentNode.style.width = this.stageWidth + 'px';
+      }
+      guideHeight = guideHeight * this.multiple;
+      this.stageWidth = parseFloat(this.t.width);
+      this.stageHeight = parseFloat(this.t.height);
+    }
+    ,
     _getClient: function () {
       var client = document.documentElement;
       return {
         width: client.clientWidth,
         height: client.clientHeight
       }
-    },
+    }
+    ,
     _bindEvent: function () {
       var _this = this;
       window.addEventListener('resize', function () {
         _this.reSetSize();
       });
-    },
+    }
+    ,
     createShape: function () {
       return new createjs.Shape();
-    },
+    }
+    ,
   };
   canvas = document.getElementById(id);
   stage = new stage(canvas);
@@ -56,10 +70,9 @@
   var userInfo = stage.createShape();
   top.addChild(userInfo);
   userInfo.alpha = 0.3;
-  userInfo.graphics.beginFill('#000').drawRoundRect (0,0,stage.stageWidth-guide*2,guideHeight,15);
+  userInfo.graphics.beginFill('#000').drawRoundRect(0, 0, stage.stageWidth - guide * 2, guideHeight, 15);
   top.x = guide;
   stage.Stage.addChild(top);
-
 
 
   var menu = new createjs.Container();
@@ -67,51 +80,66 @@
   menu.addChild(menuShape);
   menuShape.id = 'menu';
   menuShape.alpha = 0.3;
-  menuShape.graphics.beginFill('#000').drawRoundRect (0,0,stage.stageWidth-guide*2,guideHeight,15);
+  menuShape.graphics.beginFill('#000').drawRoundRect(0, 0, stage.stageWidth - guide * 2, guideHeight, 15);
   menu.x = guide;
-  menu.y = stage.stageHeight-guideHeight-5;
+  menu.y = stage.stageHeight - guideHeight - 5;
   var menuBtn = new Image();
   menuBtn.src = 'common.png';
-  menuBtn.onload = function(){
+  menuBtn.onload = function () {
     menuBtn = new createjs.Bitmap(menuBtn);
     var btn = [
-        //勇士
-      {x : 802,y : 146,callback:function(){
+      //勇士
+      {
+        x: 802, y: 146, callback: function () {
         alert('勇士');
-      }},
-        //士兵
-      {x:934,y:72,callback:function(){
+      }
+      },
+      //士兵
+      {
+        x: 934, y: 72, callback: function () {
         alert('士兵');
-      }},
-        //装备
-      {x:873, y:144,callback:function(){
+      }
+      },
+      //装备
+      {
+        x: 873, y: 144, callback: function () {
         alert('装备');
-      }},
+      }
+      },
       //排行'
-      {x:934,y:0,callback:function(){
+      {
+        x: 934, y: 0, callback: function () {
         alert('排行');
-      }},
-        //成就
-      {x:701, y:222,callback:function(){
+      }
+      },
+      //成就
+      {
+        x: 701, y: 222, callback: function () {
         alert('成就');
-      }},
+      }
+      },
       //商店
-      {x:946, y:144,callback:function(){
+      {
+        x: 946, y: 144, callback: function () {
         alert('商店');
-      }}
+      }
+      }
     ];
     var rect;
-    for(var i = 0;i<btn.length;i++){
-      rect = new createjs.Rectangle(btn[i].x,btn[i].y,72,72);
+    var btnSize = 72 * stage.multiple;
+    var offset =  (stage.stageWidth - guide * 2) / 6;
+    for (var i = 0; i < btn.length; i++) {
+      rect = new createjs.Rectangle(btn[i].x, btn[i].y, 72, 72);
       var _thisBtn = menuBtn.clone();
       _thisBtn.sourceRect = rect;
-      _thisBtn.y = guideHeight /2 - (72/2);
-      _thisBtn.x = parseInt(menuBtnOffset*(i+1) + (i*72));
+      _thisBtn.y = guideHeight / 2 - (btnSize / 2);
+      _thisBtn.x = parseInt((menuBtnOffset * stage.multiple) * (i + 1) + (i * btnSize));
+      _thisBtn.scaleX = _thisBtn.scaleY = stage.multiple;
       _thisBtn.callback = btn[i].callback;
       menu.addChild(_thisBtn);
     }
-    menu.addEventListener('click',function(e){
-      if(e.target.callback)e.target.callback();
+    menu.addEventListener('click', function (e) {
+      if (e.target.callback)e.target.callback();
     })
   };
   var textShape = stage.createShape();
@@ -126,4 +154,5 @@
     textShape.graphics.beginFill('#ff0').drawRect(-10, -10, text.getMeasuredWidth() + 10, text.getMeasuredHeight() + 10);
   });
   createjs.Ticker.addEventListener('tick', stage.Stage)
-}(window, 'canvas');
+}
+(window, 'canvas');
